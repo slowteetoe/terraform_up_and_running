17c56c8f856471f5a3dd530d2296cbc4d65e1b07
Steven Lotito <steven.lotito@gmail.com>
2020-01-04 09:13:35 -0800

    need to ignore final snapshot to destroy, or provide name

--- stage/data-stores/mysql/main.tf
+++ stage/data-stores/mysql/main.tf
@@ -1,11 +1,11 @@
 terraform {
   backend "s3" {
     bucket = "slowteetoeterraformupandrunningstate"
-    key = "stage/data-stores/mysql/terraform.tfstate"
+    key    = "stage/data-stores/mysql/terraform.tfstate"
     region = "us-west-2"
 
     dynamodb_table = "terraform-up-and-running-locks"
-    encrypt= true
+    encrypt        = true
   }
 }
 
@@ -14,13 +14,14 @@ provider "aws" {
 }
 
 resource "aws_db_instance" "example" {
-  identifier_prefix = "terraform-up-and-running"
-  engine            = "mysql"
-  allocated_storage = 10
-  instance_class    = "db.t2.micro"
-  name              = "example_database"
-  username          = "admin"
-  password          = data.aws_secretsmanager_secret_version.db_password.secret_string
+  identifier_prefix   = "terraform-up-and-running"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t2.micro"
+  name                = "example_database"
+  username            = "admin"
+  password            = data.aws_secretsmanager_secret_version.db_password.secret_string
+  skip_final_snapshot = true
 }
 
 data "aws_secretsmanager_secret_version" "db_password" {
