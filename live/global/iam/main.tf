02b27e087a3c6203573f0e6bc1c205abd57bb85c
Steven Lotito <steven.lotito@gmail.com>
2020-01-26 09:24:54 -0800

    better way to loop, probably always want for_each

--- live/global/iam/main.tf
+++ live/global/iam/main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_iam_user" "example" {
-  count = length(var.user_names)
-  name  = var.user_names[count.index]
-}
+# resource "aws_iam_user" "example" {
+#   count = length(var.user_names)
+#   name  = var.user_names[count.index]
+# }
 
 # note: internally TF will do something like 
 # * aws_iam_user.example[0]: josecuervo
@@ -14,3 +14,8 @@ resource "aws_iam_user" "example" {
 # If we were to pass in a different sized list, for example deleting johnnywalker
 # we would see TF delete the third entry and rename the second from johnnywalker -> jackdaniels
 # which is _probably_ not what we wanted
+
+resource "aws_iam_user" "example" {
+    for_each = toset(var.user_names)
+    name = each.value
+}
