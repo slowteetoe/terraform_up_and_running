5ee7ecb53383085559d2e0dec9a5c4b0d8d2771d
Steven Lotito <steven.lotito@gmail.com>
2020-02-01 10:12:32 -0800

    upgrade prod to v0.0.4, enable autoscaling

--- live/prod/services/webserver-cluster/main.tf
+++ live/prod/services/webserver-cluster/main.tf
@@ -2,9 +2,41 @@ provider "aws" {
   region = "us-west-2"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "slowteetoeterraformupandrunningstate"
+    key    = "prod/services/webserver-cluster/terraform.tfstate"
+    region = "us-west-2"
+
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
 module "webserver_cluster" {
-  source       = "../../../modules/services/webserver-cluster"
+  source       = "github.com/slowteetoe/terraform_up_and_running_modules//services/webserver-cluster?ref=v0.0.4"
+
   cluster_name = "webservers-prod"
-  bucket       = "slowteetoeterraformupandrunningstate"
-  key          = "prod/data-stores/mysql/terraform.tfstate"
+  instance_type      = "t2.micro"
+  min_size           = 2
+  max_size           = 4
+  db_host            = data.terraform_remote_state.db.outputs.db_host
+  db_port            = data.terraform_remote_state.db.outputs.db_port
+  enable_autoscaling = true
+
+  custom_tags = {
+    Owner      = "me"
+    DeployedBy = "terraform"
+  }
+}
+
+# read in the data from the provisioned mysql node
+data "terraform_remote_state" "db" {
+  backend = "s3"
+
+  config = {
+    bucket = "slowteetoeterraformupandrunningstate"
+    key    = "prod/data-stores/mysql/terraform.tfstate"
+    region = "us-west-2"
+  }
 }
